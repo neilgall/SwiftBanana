ac4f246c9a74fdb98edf170c20b41de276b7fc4b
Neil Gall <neilgall@mac.com>
2015-07-18 11:49:55 +0100

    Initial Commit

---
+++ SwiftBanana/SwiftBanana.h
@@ -0,0 +1,19 @@
+//
+//  SwiftBanana.h
+//  SwiftBanana
+//
+//  Created by Neil Gall on 18/07/2015.
+//  Copyright © 2015 Neil Gall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftBanana.
+FOUNDATION_EXPORT double SwiftBananaVersionNumber;
+
+//! Project version string for SwiftBanana.
+FOUNDATION_EXPORT const unsigned char SwiftBananaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftBanana/PublicHeader.h>
+
+
